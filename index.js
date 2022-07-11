f58a6983c225dd56d1dc636ab49de899ec84ade8
ruspokrovsky <ruspokrovsky@gmail.com>
2022-07-11 19:36:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
+// IMPORTS FROM PACKAGES
 const express = require("express");
-const PORT = process.env.PORT || 3000;
-const app = express();
 const mongoose = require("mongoose");
-const DB = "mongodb+srv://ruspokrovsky:nefaz1975@cluster0.nwuz1.mongodb.net/?retryWrites=true&w=majority"
-
-const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/admin");
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 
+// INIT
+const PORT = process.env.PORT || 3000;
+const app = express();
+const DB = "mongodb+srv://ruspokrovsky:nefaz1975@cluster0.nwuz1.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
 app.use(express.json());
 app.use(authRouter);
 app.use(adminRouter);
 app.use(productRouter);
 app.use(userRouter);
 
-
-
-mongoose.connect(DB).then(() => {
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
     console.log("Connection Successful");
-  }
-  )
+  })
   .catch((e) => {
     console.log(e);
   });
 
-
-
-app.listen(PORT, () => {
-console.log("server running port:", PORT);
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
